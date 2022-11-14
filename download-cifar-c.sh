306285c84c7fc1694fd0a3eca0594f8fea03a8ae
Kelechi Uhegbu <34874074+kelechiu10@users.noreply.github.com>
2022-11-14 10:09:22 +0000

    download scripts for cifar and imagenet -C versions

---
+++ download-cifar-c.sh
@@ -0,0 +1,1 @@
+curl -O -L https://zenodo.org/record/2535967/files/CIFAR-10-C.tar
--- download-imagenet-c.sh
+++ download-imagenet-c.sh
@@ -1,5 +1,5 @@
-curl -O -L https://zenodo.org/record/2235448/files/blur.tar?download=1
-curl -O -L https://zenodo.org/record/2235448/files/digital.tar?download=1
-curl -O -L https://zenodo.org/record/2235448/files/extra.tar?download=1
-curl -O -L https://zenodo.org/record/2235448/files/noise.tar?download=1
-curl -O -L https://zenodo.org/record/2235448/files/weather.tar?download=1
+curl -L -o blur.tar https://zenodo.org/record/2235448/files/blur.tar?download=1
+curl -L -o digital.tar https://zenodo.org/record/2235448/files/digital.tar?download=1
+curl -L -o extra.tar https://zenodo.org/record/2235448/files/extra.tar?download=1
+curl -L -o noise.tar https://zenodo.org/record/2235448/files/noise.tar?download=1
+curl -L -o weather.tar https://zenodo.org/record/2235448/files/weather.tar?download=1
