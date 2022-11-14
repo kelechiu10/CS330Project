46eef3ea911e40ca64efe66147e1f02174a1844f
Kelechi Uhegbu <34874074+kelechiu10@users.noreply.github.com>
2022-11-14 00:34:46 -0800

    Download script for all imagenet-c data

---
+++ download-imagenet-c.sh
@@ -0,0 +1,5 @@
+curl -O -L https://zenodo.org/record/2235448/files/blur.tar?download=1
+curl -O -L https://zenodo.org/record/2235448/files/digital.tar?download=1
+curl -O -L https://zenodo.org/record/2235448/files/extra.tar?download=1
+curl -O -L https://zenodo.org/record/2235448/files/noise.tar?download=1
+curl -O -L https://zenodo.org/record/2235448/files/weather.tar?download=1
